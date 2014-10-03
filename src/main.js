0bd20d7c06905a48c06d15d6025630fc04c5b198
Justin Walgran <jwalgran@azavea.com>
2014-10-03 08:51:25 -0400

    Temporarily put the db in the global namespace
    This helps with testing from the console.

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ var config = require('./config');
 var App = require('./App.jsx');
 var Database = require('./Database.js');
 
-var db = new Database(config.localDb, config.serverDb);
+//TODO: PUT THE 'var' BACK
+db = new Database(config.localDb, config.serverDb);
 React.renderComponent(App({db: db}), document.getElementById('content'));
