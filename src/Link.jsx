a6e46d80ee0b7eb8fb978bf62d7f70c69657e000
Justin Walgran <jwalgran@azavea.com>
2015-01-02 16:30:40 -0700

    Update Link to include attrs and preventDefault
    The goal, when using the Link component, is to run a function rather
    than the default anchor tag action, so it makes sense to always call
    preventDefault. Passing through the id and CSS class helps with styling
    the anchor.

--- src/Link.jsx
+++ src/Link.jsx
@@ -4,11 +4,12 @@ var _ = require('lodash');
 var Link = React.createClass({
     render: function() {
         var handler = function(e) {
+            e.preventDefault();
             this.props.action(this.props.value);
         }.bind(this);
 
         return (
-            <a href="#" onClick={handler}>{this.props.children}</a>
+            <a href="#" id={this.props.id} className={this.props.className} onClick={handler}>{this.props.children}</a>
         );
     }
 });
